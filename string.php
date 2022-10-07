ea94331cfd983b66acd965a90d57eae3ecd861cd
arsyaya <arsyajr9@gmail.com>
2022-10-07 11:36:07 +0700

    Php-Pertama

---
+++ string.php
@@ -0,0 +1,27 @@
+<?php
+
+//awalan
+ $nama = "Arsya Rizvadwinafisa";
+ echo $nama;
+ echo "</br>";
+
+ //strlen() 
+ echo strlen($nama);
+ echo "</br>";
+
+ //str_word_count
+ echo str_word_count($nama);
+ echo "</br>";
+
+ //strrev
+  echo strrev($nama);
+  echo "</br>";
+ 
+  //strpos
+  echo strpos($nama, "Rizvadwinafisa");
+  echo "</br>";
+
+  echo str_replace("Rizvadwinafisa", "Bimbim", $nama );
+
+
+?>
